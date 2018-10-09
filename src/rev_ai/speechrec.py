becee366f47429dbd2bcb74cea4376a6b5804eb0
Joshua <jdong42@gmail.com>
2018-10-09 02:10:08 -0500

    initial refactor

--- src/rev_ai/speechrec.py
+++ src/rev_ai/speechrec.py
@@ -1,64 +1,104 @@
 # -*- coding: utf-8 -*-
 """Speech recognition tools for using rev.ai
 """
-import requests
+try:
+    from urllib.parse import urljoin
+except ImportError:
+    from urlparse import urljoin
 import os
+import requests
+import logging
+
+LOG = logging.getLogger(__name__)
 
 
 class RevSpeechAPI:
-    BASE_URL = "https://api.rev.ai/revspeech/{version}".format(version="v1beta")
+    VERSION = "v1beta"
+    BASE_URL = "https://api.rev.ai/revspeech/{v}/".format(v=VERSION)
 
     def __init__(self, api_key):
         if not api_key:
-            raise ValueError("API Key cannot be empty")
-        self.HEADERS = {'Authorization':
-                        'Bearer {api_key}'.format(api_key=api_key)}
+            raise ValueError("API Key cannot be empty.")
+        self.s = requests.Session()
+        self.s.headers.update({
+            'Authorization': 'Bearer {api_key}'.format(api_key=api_key)
+        })
 
-    def submit_job_url(self, media_url, metadata=" "):
-        url = os.path.join(self.BASE_URL, "jobs")
-        payload = {'media_url': media_url,
-                   'metadata': metadata}
-        request = requests.post(url, headers=self.HEADERS, json=payload)
+    def submit_job_url(self, media_url, metadata=""):
+        """Submit a web URL for transcription.
+        The audio data is downloaded from the URL.
 
-        response_body = request.json()
-        return response_body
+        :param media_url: web location of the media file
+        :param metadata: info to associate with the transcription job
+        :returns: raw response data
+        """
+        url_jobs = urljoin(self.BASE_URL, "jobs")
+        payload = {
+            'media_url': media_url,
+            'metadata': metadata
+        }
 
-    def submit_job_local_file(self, file, media_type="audio"):
-        url = os.path.join(self.BASE_URL, "jobs")
-        if media_type != "audio" and media_type != "video":
+        response = self.s.post(url_jobs, json=payload)
+
+        return response.json()
+
+    def submit_job_local_file(self, filename,
+                              media_type="audio", content_type=None):
+        """Submit a local file for transcription.
+        Note that the content type is inferred if not provided.
+
+        :param filename: path to a local file on disk
+        :param media_type: "audio" or "video"
+        :param content_type: explicitly specify request content type
+        :returns: raw response data
+        """
+        url_jobs = urljoin(self.BASE_URL, "jobs")
+        if media_type not in {"audio", "video"}:
             raise ValueError("media_type must be audio or video")
-        filename, file_extension = os.path.splitext(file)
-        with open(file, 'rb') as fo:
-            files = {'media': (file, fo,  media_type + '/' + file_extension)}
-            request = requests.post(url, headers=self.HEADERS, files=files)
+        _base, extension = os.path.splitext(filename)
+        content_type = content_type or media_type + '/' + extension
+        LOG.debug('Using content type: "%s".', content_type)
 
-        response_body = request.json()
-        return response_body
+        with open(filename, 'rb') as f:
+            files = {
+                'media': (filename, f, content_type)
+            }
+            response = self.s.post(url_jobs, files=files)
 
-    def view_job(self, id):
-        url = os.path.join(self.BASE_URL, "jobs", id)
-        request = requests.get(url, headers=self.HEADERS)
+        return response.json()
 
-        response_body = request.json()
-        return response_body
+    def view_job(self, id_):
+        """View information about a specific job.
+        The server will respond with the status and creation date.
 
-    def get_transcript(self, id, response_type=".json"):
-        url = os.path.join(self.BASE_URL, "jobs", id, 'transcript')
-        headers = self.HEADERS.copy()
+        :param id_: id that the server gave you
+        :returns: raw response data
+        """
+        url_jobs_id = urljoin(self.BASE_URL, "jobs/{id_}".format(id_=id_))
+
+        response = self.s.get(url_jobs_id)
+
+        return response.json()
+
+    def get_transcript(self, id_, response_type=".json"):
+        url_jobs_transcript = urljoin(self.BASE_URL,
+                                      "jobs/{id_}/transcript".format(id_=id_))
         if response_type == ".json":
-            headers['Accept'] = 'application/{version}+json'.format(version="vnd.rev.transcript.v1.0")
+            content_type_accept = ('application/{version}+json'
+                                   .format(version="vnd.rev.transcript.v1.0"))
         elif response_type == ".txt":
-            headers['Accept'] = 'text/plain'
+            content_type_accept = 'text/plain'
         else:
-            raise ValueError("response_type must be .json or .txt")
-        request = requests.get(url, headers=headers)
+            raise ValueError("response_type must be .json or .txt!")
 
-        response_body = request.json() if response_type == ".json" else request.text
-        return response_body
+        response = self.s.get(url_jobs_transcript,
+                              headers={'Accept': content_type_accept})
+
+        return response.json() if response_type == ".json" else response.text
 
     def get_account(self):
-        url = os.path.join(self.BASE_URL, "account")
-        request = requests.get(url, headers=self.HEADERS)
+        url_account = urljoin(self.BASE_URL, "account")
 
-        response_body = request.json()
-        return response_body
+        response = self.s.get(url_account)
+
+        return response.json()
