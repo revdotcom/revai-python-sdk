5fe93c51a298e714f9eacc2ee8f7096e63b8d9c7
kbridbur <kbridbur@mit.edu>
2019-01-16 15:01:41 -0800

    Replace travis badge link

--- src/rev_ai/models/job_options.py
+++ src/rev_ai/models/job_options.py
@@ -3,7 +3,7 @@
 
 
 class JobSubmitOptions:
-    def __init__(self,  metadata=None, callback_url=None, skip_diarization=None):
+    def __init__(self,  metadata=None, callback_url=None, skip_diarization=False):
         """
         :param metadata: info to associate with the transcription job
         :param callback_url: callback url to invoke on job completion as a webhook
@@ -11,4 +11,4 @@ class JobSubmitOptions:
         """
         self.metadata = "" if metadata is None else metadata
         self.callback_url = callback_url
-        self.skip_diarization = skip_diarization is not None
+        self.skip_diarization = skip_diarization
