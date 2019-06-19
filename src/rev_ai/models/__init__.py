5798021514dfa869b2dd3b0fea31725a2d064ba9
Ymardini <yousef.mardini@rev.com>
2019-06-19 13:54:01 -0700

    back

--- src/rev_ai/models/__init__.py
+++ src/rev_ai/models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-# from .async import Job, JobStatus, Account, Transcript, Monologue, Element
-# from .streaming import MediaConfig
+from .async import Job, JobStatus, Account, Transcript, Monologue, Element
+from .streaming import MediaConfig
