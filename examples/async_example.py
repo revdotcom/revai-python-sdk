159880b86514c096a33a545914c8a10a8a6a71ac
Ymardini <yousef.mardini@rev.com>
2019-06-28 16:55:46 -0700

    edits

--- examples/async_example.py
+++ examples/async_example.py
@@ -25,18 +25,20 @@ access_token = "your_access_token"
 client = apiclient.RevAiAPIClient(access_token)
 
 # Submitting a job through a local file. The optional parameters are shown below.
-job = client.submit_job_local_file("english_test.raw",
-                                   metadata=None,
-                                   callback_url=None,
-                                   skip_diarization=False,
-                                   custom_vocabularies=None)
+# job = client.submit_job_local_file("your_local_file_path",
+#                                    metadata=None,
+#                                    callback_url=None,
+#                                    skip_diarization=False,
+#                                    custom_vocabularies=None)
 
 # Submitting a job with a link to the file you want transcribed
-# job = client.submit_job_url("your_url",
-#                             metadata=None,
-#                             callback_url=None,
-#                             skip_diarization=False,
-#                             custom_vocabularies=None)
+# Change url to your url
+url = "https://www.rev.ai/FTC_Sample_1.mp3"
+job = client.submit_job_url(url,
+                            metadata=None,
+                            callback_url=None,
+                            skip_diarization=False,
+                            custom_vocabularies=None)
 
 print("Submitted Job")
 
@@ -53,28 +55,30 @@ while True:
         time.sleep(5)
         continue
 
-    elif status == "FAILURE":
-        print("Transcription Failed")
+    elif status == "FAILED":
+        print("Job Failed : {}".format(job_details.failure_detail))
         break
-        
-    break
 
-# Getting a list of current jobs connected with your account
-# The optional parameters limits limits the length of the list. Starting_after
-# Cuts off the beginning x jobs off the list returned
-list_of_jobs = client.get_list_of_jobs(limit=None, starting_after=None)
+    if status == "TRANSCRIBED":
+      # Getting a list of current jobs connected with your account
+      # The optional parameters limits limits the length of the list. Starting_after
+      # Cuts off the beginning x jobs off the list returned
+      list_of_jobs = client.get_list_of_jobs(limit=None, starting_after=None)
 
-# obtain transcript text as a string for the job.
-transcript_text = client.get_transcript_text(job.id)
+      # obtain transcript text as a string for the job.
+      transcript_text = client.get_transcript_text(job.id)
+      print(transcript_text)
 
-# obtain transcript text as a json object for the job.
-transcript_json = client.get_transcript_json(job.id)
+      # obtain transcript text as a json object for the job.
+      transcript_json = client.get_transcript_json(job.id)
 
-# obtain transcript object for the job.
-transcript_obj = client.get_transcript_object(job.id)
+      # obtain transcript object for the job.
+      transcript_obj = client.get_transcript_object(job.id)
 
-# obtain captions for the job.
-captions = client.get_captions(job.id)
+      # obtain captions for the job.
+      captions = client.get_captions(job.id)
+
+      break
 
 # Use the objects however you please
 
@@ -82,4 +86,4 @@ captions = client.get_captions(job.id)
 # NOTE : This will PERMANENTLY DELETE all data related to a job. Exercise only
 # if you're sure you want to delete the job.
 # client.delete_job(job.id)
-print("Transcription complete.")
+print("Job Submission and Collection Finished.")
