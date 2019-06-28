728ad1d964a24cf7223e57750bbadd2ef40d32c2
Ymardini <yousef.mardini@rev.com>
2019-06-28 16:29:15 -0700

    AISDK-48: Fix examples for python SDK

--- examples/async_example.py
+++ examples/async_example.py
@@ -25,41 +25,44 @@ access_token = "your_access_token"
 client = apiclient.RevAiAPIClient(access_token)
 
 # Submitting a job through a local file. The optional parameters are shown below.
-job = client.submit_job_local_file("your_local_file_path",
+job = client.submit_job_local_file("english_test.raw",
                                    metadata=None,
                                    callback_url=None,
                                    skip_diarization=False,
                                    custom_vocabularies=None)
 
 # Submitting a job with a link to the file you want transcribed
-# job = client.submit_job_url("your_local_file_path",
+# job = client.submit_job_url("your_url",
 #                             metadata=None,
-#                             allback_url=None,
+#                             callback_url=None,
 #                             skip_diarization=False,
 #                             custom_vocabularies=None)
+
+print("Submitted Job")
 
 while True:
 
     # Obtains details of a job in json format
     job_details = client.get_job_details(job.id)
-    details_dict = json.loads(job_details)
-    status = details_dict["status"]
+    status = job_details.status.name
+
+    print("Job Status : {}".format(status))
 
     # Checks if the job has been transcribed
-    if status == "in_progress":
+    if status == "IN_PROGRESS":
         time.sleep(5)
         continue
-    elif status == "failure":
 
-        # Do something if the job failed
-
+    elif status == "FAILURE":
+        print("Transcription Failed")
         break
+        
     break
 
 # Getting a list of current jobs connected with your account
 # The optional parameters limits limits the length of the list. Starting_after
 # Cuts off the beginning x jobs off the list returned
-list_of_jobs = client.get_list_of_jobs(limits=None, starting_after=None)
+list_of_jobs = client.get_list_of_jobs(limit=None, starting_after=None)
 
 # obtain transcript text as a string for the job.
 transcript_text = client.get_transcript_text(job.id)
@@ -78,4 +81,5 @@ captions = client.get_captions(job.id)
 # Once you are done with the job, you can delete it.
 # NOTE : This will PERMANENTLY DELETE all data related to a job. Exercise only
 # if you're sure you want to delete the job.
-client.delete_job(job.id)
+# client.delete_job(job.id)
+print("Transcription complete.")
