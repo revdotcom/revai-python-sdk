608af7970bdac708bbd7d0f52f5b380b00128f7b
kbridbur <kbridbur@mit.edu>
2019-08-12 13:25:56 -0700

    Fix iport

--- src/rev_ai/models/asynchronous/__init__.py
+++ src/rev_ai/models/asynchronous/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Async Models"""
 
-from .caption_types import CaptionType
+from .caption_type import CaptionType
 from .job import Job
 from .job_status import JobStatus
 from .account import Account
