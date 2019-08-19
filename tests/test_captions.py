9a8055c37ceb5af2e57f82b30d4f1db4b5e41a0c
kbridbur <kbridbur@mit.edu>
2019-08-19 15:41:19 -0700

    More testing

--- tests/test_captions.py
+++ tests/test_captions.py
@@ -26,7 +26,24 @@ class TestCaptionEndpoint():
         mock_client.session.request.return_value = response
 
         res = mock_client.get_captions(JOB_ID)
-        print(expected_content_type)
+        assert res == data
+        mock_client.session.request.assert_called_once_with(
+            "GET",
+            URL,
+            headers={'Accept': expected_content_type}
+        )
+
+    @pytest.mark.parametrize('content_type', [CaptionType.SRT, CaptionType.VTT])
+    def test_get_captions_with_content_type(self, content_type, mock_client, make_mock_response):
+        data = 'Test'
+        if sys.version_info > (3, 0):
+            expected_content_type = content_type.value
+        else:
+            expected_content_type = content_type.SRT
+        response = make_mock_response(url=URL, text=data)
+        mock_client.session.request.return_value = response
+
+        res = mock_client.get_captions(JOB_ID, content_type)
         assert res == data
         mock_client.session.request.assert_called_once_with(
             "GET",
@@ -50,6 +67,25 @@ class TestCaptionEndpoint():
 
         res = mock_client.get_captions_as_stream(JOB_ID)
 
+        assert res.content.decode('utf-8') == data
+        mock_client.session.request.assert_called_once_with(
+            "GET",
+            URL,
+            headers={'Accept': expected_content_type},
+            stream=True
+        )
+
+    @pytest.mark.parametrize('content_type', [CaptionType.SRT, CaptionType.VTT])
+    def test_get_captions_as_stream_with_content_type(self, content_type, mock_client, make_mock_response):
+        data = 'Test'
+        if sys.version_info > (3, 0):
+            expected_content_type = content_type.value
+        else:
+            expected_content_type = content_type.SRT
+        response = make_mock_response(url=URL, text=data)
+        mock_client.session.request.return_value = response
+
+        res = mock_client.get_captions_as_stream(JOB_ID, content_type)
         assert res.content.decode('utf-8') == data
         mock_client.session.request.assert_called_once_with(
             "GET",
