671a5db161ce5806fc47b5d4a7c3f0bb095f3f46
Kostas Moustaklem <kostas.moustaklem@rev.com>
2019-10-31 13:22:45 -0500

    PEP8

--- src/rev_ai/utils.py
+++ src/rev_ai/utils.py
@@ -3,7 +3,8 @@
 
 from . import CustomVocabulary
 
+
 def _process_vocabularies(unprocessed_vocabularies):
-        return list(map(lambda custom_vocabulary: custom_vocabulary.to_dict()
-                        if isinstance(custom_vocabulary, CustomVocabulary)
-                        else custom_vocabulary, unprocessed_vocabularies))
+    return list(map(lambda custom_vocabulary: custom_vocabulary.to_dict()
+                    if isinstance(custom_vocabulary, CustomVocabulary)
+                    else custom_vocabulary, unprocessed_vocabularies))
