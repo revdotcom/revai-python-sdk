a9cb65aac1c32f35a183e607aa4f35375f335d99
Kostas Moustaklem <kostas.moustaklem@rev.com>
2019-11-01 11:00:47 -0500

    Docstring for _process_vocabs

--- src/rev_ai/utils.py
+++ src/rev_ai/utils.py
@@ -5,6 +5,12 @@ from . import CustomVocabulary
 
 
 def _process_vocabularies(unprocessed_vocabularies):
+    """
+    This method takes in a list that contains CustomVocabulary objects
+    and returns a list in which any such objects are converted properly
+    to custom vocabulary dictionaries. Any items of other types in the list
+    are not changed.
+    """
     return list(map(lambda custom_vocabulary: custom_vocabulary.to_dict()
                     if isinstance(custom_vocabulary, CustomVocabulary)
                     else custom_vocabulary, unprocessed_vocabularies))
