ec0e22874115306f49c219a51c44559234fac278
Kevin Weng <kevinweng@rev.com>
2020-07-22 13:37:00 -0500

    AISDK-122: Fix import and requirements issues in Python setup (#55)

--- setup.py
+++ setup.py
@@ -2,15 +2,35 @@
 # -*- coding: utf-8 -*-
 from glob import glob
 from setuptools import setup, find_packages
-try:  # for pip >= 10
+try:
+    # pip >=20
+    from pip._internal.network.session import PipSession
     from pip._internal.req import parse_requirements
-    from pip._internal.download import PipSession
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
-    from pip.download import PipSession
+except ImportError:
+    try:
+        # 10.0.0 <= pip <= 19.3.1
+        from pip._internal.download import PipSession
+        from pip._internal.req import parse_requirements
+    except ImportError:
+        # pip <= 9.0.3
+        from pip.download import PipSession
+        from pip.req import parse_requirements
 import re
 import ast
 import os
+
+
+def get_requirements(parsed_requirements):
+    """Return strings of requirements from pip's ParsedRequirement objects
+
+    In pip <= 19.3.1, requirements are in ParsedRequirement.req
+    In pip > 19.3.1, requirements are in ParsedRequirement.requirement
+    """
+    return [
+        str(ir.req if hasattr(ir, "req") else ir.requirement)
+        for ir in parsed_requirements
+    ]
+
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
@@ -24,14 +44,8 @@ with open('README.md') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-parsed_requirements = parse_requirements(
-    'requirements.txt',
-    session=PipSession())
-parsed_test_requirements = parse_requirements(
-    'requirements_dev.txt',
-    session=PipSession())
-requirements = [str(ir.req) for ir in parsed_requirements]
-test_requirements = [str(tr.req) for tr in parsed_test_requirements]
+parsed_requirements = parse_requirements('requirements.txt', session=PipSession())
+parsed_test_requirements = parse_requirements('requirements_dev.txt', session=PipSession())
 
 setup(
     name='rev_ai',
@@ -45,7 +59,7 @@ setup(
     py_modules=[os.path.splitext(os.path.basename(path))[0]
                 for path in glob('src/*.py')],
     include_package_data=True,
-    install_requires=requirements,
+    install_requires=get_requirements(parsed_requirements),
     zip_safe=False,
     license='MIT license',
     keywords='rev_ai',
@@ -64,6 +78,6 @@ setup(
     ],
     setup_requires=['pytest-runner==4.2'],
     test_suite='tests',
-    tests_require=test_requirements,
+    tests_require=get_requirements(parsed_test_requirements),
     url='https://github.com/revdotcom/revai-python-sdk',
 )
