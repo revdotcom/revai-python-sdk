cd31c09921166780c522470fb52d32c3e2254d7e
Kevin Weng <kevinweng@rev.com>
2020-07-22 12:17:57 -0500

    actual fix

--- setup.py
+++ setup.py
@@ -2,11 +2,30 @@
 # -*- coding: utf-8 -*-
 from glob import glob
 from setuptools import setup, find_packages
-from pip._internal.req import parse_requirements
-from pip._internal.network.session import PipSession
+try:
+    # pip >=20
+    from pip._internal.network.session import PipSession
+    from pip._internal.req import parse_requirements
+except ImportError:
+    try:
+        # 10.0.0 <= pip <= 19.3.1
+        from pip._internal.download import PipSession
+        from pip._internal.req import parse_requirements
+    except ImportError:
+        # pip <= 9.0.3
+        from pip.download import PipSession
+        from pip.req import parse_requirements
 import re
 import ast
 import os
+
+
+def get_requirements(parsed_requirements):
+    return [
+        str(ir.req if hasattr(ir, "req") else ir.requirement)
+        for ir in parsed_requirements
+    ]
+
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
@@ -20,14 +39,8 @@ with open('README.md') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-parsed_requirements = parse_requirements(
-    'requirements.txt',
-    session=PipSession())
-parsed_test_requirements = parse_requirements(
-    'requirements_dev.txt',
-    session=PipSession())
-requirements = [str(ir.req) for ir in parsed_requirements]
-test_requirements = [str(tr.req) for tr in parsed_test_requirements]
+parsed_requirements = parse_requirements('requirements.txt', session=PipSession())
+parsed_test_requirements = parse_requirements('requirements_dev.txt', session=PipSession())
 
 setup(
     name='rev_ai',
@@ -41,7 +54,7 @@ setup(
     py_modules=[os.path.splitext(os.path.basename(path))[0]
                 for path in glob('src/*.py')],
     include_package_data=True,
-    install_requires=requirements,
+    install_requires=parsed_requirements(parsed_requirements),
     zip_safe=False,
     license='MIT license',
     keywords='rev_ai',
@@ -60,6 +73,6 @@ setup(
     ],
     setup_requires=['pytest-runner==4.2'],
     test_suite='tests',
-    tests_require=test_requirements,
+    tests_require=parsed_requirements(parsed_test_requirements),
     url='https://github.com/revdotcom/revai-python-sdk',
 )
