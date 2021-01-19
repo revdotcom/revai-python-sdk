46ce381bc63a6746dd1fc83b63c636851cdce840
Andrew Mikofalvy <amikofalvy@rev.com>
2021-01-18 16:33:28 -0800

    Increase version number to 2.11.0

--- src/rev_ai/__init__.py
+++ src/rev_ai/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Top-level package for rev_ai"""
 
-__version__ = '2.10.1'
+__version__ = '2.11.0'
 
 from .models import Job, JobStatus, Account, Transcript, MediaConfig, CaptionType, CustomVocabulary
