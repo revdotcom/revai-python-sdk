84dd8ba89c24b370a5bd5b93623384d2b3d6b1b2
Henry Jue <henry.jue@rev.com>
2021-08-13 09:39:08 -0700

    bumping version

--- src/rev_ai/__init__.py
+++ src/rev_ai/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Top-level package for rev_ai"""
 
-__version__ = '2.12.1'
+__version__ = '2.13.0'
 
 from .models import Job, JobStatus, Account, Transcript, MediaConfig, CaptionType, CustomVocabulary
