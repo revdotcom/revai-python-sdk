015b2f3c211f824728e24b3601c3348c944afd4d
Aaron Wilson <aaron.wilson@rev.com>
2022-04-18 15:27:51 -0500

    formatting

--- src/rev_ai/models/customer_url_data.py
+++ src/rev_ai/models/customer_url_data.py
@@ -2,10 +2,10 @@ class CustomerUrlData:
     """Object used to contain a url provided by the customer along with any other related information,
      such as authentication headers."""
 
-    def __init__(self, url, auth_headers = None):
+    def __init__(self, url, auth_headers=None):
         """Constructor
         :param url: customer provided url
-        :param auth_headers: customer provided headers to access the url
+        :param auth_headers: optional customer provided headers to access the url
         """
         self.url = url
         self.auth_headers = auth_headers
