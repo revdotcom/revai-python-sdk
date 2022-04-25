13232fb285feab335e2721508831fd57c7c49904
Aaron Wilson <aaron.wilson@rev.com>
2022-04-25 14:52:16 -0500

    test new utils fn for exclusive options

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """Unit tests for Rev Ai Utils"""
+import pytest
 
-from src.rev_ai.utils import _process_vocabularies
+from src.rev_ai.utils import _process_vocabularies, check_exclusive_options
 from src.rev_ai.models import CustomVocabulary
 
 phrases = ["Patrick Henry Winston", "Noam Chomsky"]
@@ -34,3 +35,17 @@ class TestUtils:
         processed_vocabs = _process_vocabularies([])
 
         assert processed_vocabs == []
+
+    def test_check_exclusive_options(self):
+        option1 = "opt1"
+        option2 = "opt2"
+        option1_name = "name1"
+        option2_name = "name2"
+        # test either option
+        check_exclusive_options(None, option1_name, option2, option2_name)
+        check_exclusive_options(option1, option1_name, None, option2_name)
+        # Raise error if both
+        with pytest.raises(ValueError) as err:
+            check_exclusive_options(option1, option1_name, option2, option2_name)
+        assert "Only one of {0} or {1} may be provided".format(option1_name, option2_name) in \
+               str(err.value)
