488403e5896503fc3363c385da0a143359742df0
Aaron Wilson <aaron.wilson@rev.com>
2022-06-02 12:32:02 -0500

    remove unused errors from test

--- tests/helpers/errors.py
+++ tests/helpers/errors.py
@@ -3,16 +3,6 @@ def get_error_test_cases(errors):
 
 
 ERRORS = {
-    'invalid-parameters': {
-        "parameter": {
-            "example": [
-                "The example field is required"
-            ]
-        },
-        "type": "https://www.rev.ai/api/v1/errors/invalid-parameters",
-        "title": "Your request parameters didn't validate",
-        "status": 400
-    },
     'unauthorized': {
         "title": "Authorization has been denied for this request",
         "status": 401
@@ -21,13 +11,6 @@ ERRORS = {
         "type": "https://www.rev.ai/api/v1/errors/job-not-found",
         "title": "could not find job",
         "status": 404
-    },
-    'out-of-credit': {
-        "title": "You do not have enough credits",
-        "type": "https://www.rev.ai/api/v1/errors/out-of-credit",
-        "detail": "You have only 60 seconds remaining",
-        "current_balance": 60,
-        "status": 403
     },
     'invalid-job-state': {
         "allowed_values": [
