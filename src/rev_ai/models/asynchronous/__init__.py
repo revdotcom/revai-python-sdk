ab7ab00e10116f9f12760a6d8a4ce77f40ae4bb5
Jenny Wong <jenny.wong@rev.com>
2022-06-10 12:24:21 -0700

    typo

--- src/rev_ai/models/asynchronous/__init__.py
+++ src/rev_ai/models/asynchronous/__init__.py
@@ -5,6 +5,6 @@ from .caption_type import CaptionType
 from .job import Job
 from .job_status import JobStatus
 from .account import Account
-from .transcript import Transcript, TranscriptType, Monologue, Element
+from .transcript import Transcript, Monologue, Element
 from .transcript_type import TranscriptType
 from .speaker_name import SpeakerName
