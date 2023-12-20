d38ac85d526ce9791668ebc187700c516d2f34d1
Kirill Bykov <kirill.bykov@rev.com>
2023-12-20 13:02:23 -0600

    1

--- src/rev_ai/models/asynchronous/summary.py
+++ src/rev_ai/models/asynchronous/summary.py
@@ -1,8 +1,9 @@
+from typing import List, Dict
 class Summary:
     def __init__(
             self,
             summary:str,
-            bullet_points: list[str],
+            bullet_points: List[str],
     ):
         self.summary = summary
         self.bullet_points = bullet_points
