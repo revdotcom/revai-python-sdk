80b5e884f3b1a2751bc54c98991eb7733411ef55
Kirill Bykov <kirill.bykov@rev.com>
2024-01-03 18:18:45 -0600

    Version bump

--- src/rev_ai/__init__.py
+++ src/rev_ai/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Top-level package for rev_ai"""
 
-__version__ = '2.19.3'
+__version__ = '2.19.4'
 
 from .models import Job, JobStatus, Account, Transcript, Monologue, Element, MediaConfig, \
     CaptionType, CustomVocabulary, TopicExtractionJob, TopicExtractionResult, Topic, Informant, \
