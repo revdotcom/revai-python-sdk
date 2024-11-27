bfc7424eb3832fd9fa7e2599effc5e7f3534eb18
Dmitry Reshetnik <dmitry.reshetnik@rev.com>
2024-11-27 11:10:38 -0500

    add tests

--- tests/test_transcript.py
+++ tests/test_transcript.py
@@ -34,25 +34,57 @@ class TestTranscriptEndpoints():
         mock_session.request.assert_called_once_with("GET",
                                                      URL,
                                                      headers=expected_headers)
-
-    @pytest.mark.parametrize('id', [None, ''])
-    def test_get_transcript_text_with_no_job_id(self, id, mock_session):
-        with pytest.raises(ValueError, match='id_ must be provided'):
-            RevAiAPIClient(TOKEN).get_transcript_text(id)
-
-    def test_get_transcript_text_as_stream(self, mock_session, make_mock_response):
+        
+    @pytest.mark.parametrize('group_channels_by, group_channels_threshold_ms', [(None, None), ('sentence', 5000), ('word', 2000)])
+    def test_get_transcript_text(self, mock_session, make_mock_response, group_channels_by, group_channels_threshold_ms):
         data = 'Test'
         client = RevAiAPIClient(TOKEN)
         expected_headers = {'Accept': 'text/plain'}
         expected_headers.update(client.default_headers)
         response = make_mock_response(url=URL, text=data)
         mock_session.request.return_value = response
+        expected_url = URL
+        if group_channels_by and group_channels_threshold_ms:
+            expected_url += f"?group_channels_by={group_channels_by}&group_channels_threshold_ms={group_channels_threshold_ms}"
 
-        res = client.get_transcript_text_as_stream(JOB_ID)
+        res = client.get_transcript_text(JOB_ID, group_channels_by=group_channels_by, group_channels_threshold_ms=group_channels_threshold_ms)
+
+        assert res == data
+        mock_session.request.assert_called_once_with("GET",
+                                                     expected_url,
+                                                     headers=expected_headers)
+
+    @pytest.mark.parametrize('id', [None, ''])
+    def test_get_transcript_text_with_no_job_id(self, id, mock_session):
+        with pytest.raises(ValueError, match='id_ must be provided'):
+            RevAiAPIClient(TOKEN).get_transcript_text(id)
+
+    @pytest.mark.parametrize(
+        'group_channels_by, group_channels_threshold_ms',
+        [(None, None), ('sentence', 5000), ('word', 2000)]
+    )
+    def test_get_transcript_text_as_stream(
+        self,
+        mock_session,
+        make_mock_response,
+        group_channels_by,
+        group_channels_threshold_ms
+    ):
+        data = 'Test'
+        client = RevAiAPIClient(TOKEN)
+        expected_headers = {'Accept': 'text/plain'}
+        expected_headers.update(client.default_headers)
+        response = make_mock_response(url=URL, text=data)
+        mock_session.request.return_value = response
+        expected_url = URL
+        if group_channels_by and group_channels_threshold_ms:
+            expected_url += f"?group_channels_by={group_channels_by}&group_channels_threshold_ms={group_channels_threshold_ms}"
+
+        res = client.get_transcript_text_as_stream(JOB_ID, group_channels_by=group_channels_by, group_channels_threshold_ms=group_channels_threshold_ms)
 
         assert res.content == data
         mock_session.request.assert_called_once_with("GET",
-                                                     URL,
+                                                     expected_url,
                                                      headers=expected_headers,
                                                      stream=True)
 
@@ -61,7 +93,17 @@ class TestTranscriptEndpoints():
         with pytest.raises(ValueError, match='id_ must be provided'):
             RevAiAPIClient(TOKEN).get_transcript_text_as_stream(id)
 
-    def test_get_transcript_json(self, mock_session, make_mock_response):
+    @pytest.mark.parametrize(
+        'group_channels_by, group_channels_threshold_ms',
+        [(None, None), ('sentence', 5000), ('word', 2000)]
+    )
+    def test_get_transcript_json(
+        self,
+        mock_session,
+        make_mock_response,
+        group_channels_by,
+        group_channels_threshold_ms
+    ):
         data = {
             'monologues': [{
                 'speaker': 1,
@@ -80,19 +122,32 @@ class TestTranscriptEndpoints():
         expected_headers.update(client.default_headers)
         response = make_mock_response(url=URL, json_data=data)
         mock_session.request.return_value = response
+        expected_url = URL
+        if group_channels_by and group_channels_threshold_ms:
+            expected_url += f"?group_channels_by={group_channels_by}&group_channels_threshold_ms={group_channels_threshold_ms}"
 
-        res = client.get_transcript_json(JOB_ID)
+        res = client.get_transcript_json(JOB_ID, group_channels_by=group_channels_by, group_channels_threshold_ms=group_channels_threshold_ms)
 
         assert res == expected
         mock_session.request.assert_called_once_with(
-            "GET", URL, headers=expected_headers)
+            "GET", expected_url, headers=expected_headers)
 
     @pytest.mark.parametrize('id', [None, ''])
     def test_get_transcript_json_with_no_job_id(self, id, mock_session):
         with pytest.raises(ValueError, match='id_ must be provided'):
             RevAiAPIClient(TOKEN).get_transcript_json(id)
 
-    def test_get_transcript_json_as_stream(self, mock_session, make_mock_response):
+    @pytest.mark.parametrize(
+        'group_channels_by, group_channels_threshold_ms',
+        [(None, None), ('sentence', 5000), ('word', 2000)]
+    )
+    def test_get_transcript_json_as_stream(
+        self,
+        mock_session,
+        make_mock_response,
+        group_channels_by,
+        group_channels_threshold_ms
+    ):
         data = {
             'monologues': [{
                 'speaker': 1,
@@ -111,19 +166,32 @@ class TestTranscriptEndpoints():
         expected_headers.update(client.default_headers)
         response = make_mock_response(url=URL, json_data=data)
         mock_session.request.return_value = response
+        expected_url = URL
+        if group_channels_by and group_channels_threshold_ms:
+            expected_url += f"?group_channels_by={group_channels_by}&group_channels_threshold_ms={group_channels_threshold_ms}"
 
-        res = client.get_transcript_json_as_stream(JOB_ID)
+        res = client.get_transcript_json_as_stream(JOB_ID, group_channels_by=group_channels_by, group_channels_threshold_ms=group_channels_threshold_ms)
 
         assert json.loads(res.content.decode('utf-8').replace("\'", "\"")) == expected
         mock_session.request.assert_called_once_with(
-            "GET", URL, headers=expected_headers, stream=True)
+            "GET", expected_url, headers=expected_headers, stream=True)
 
     @pytest.mark.parametrize('id', [None, ''])
     def test_get_transcript_json_as_stream_with_no_job_id(self, id, mock_session):
         with pytest.raises(ValueError, match='id_ must be provided'):
             RevAiAPIClient(TOKEN).get_transcript_json_as_stream(id)
 
-    def test_get_transcript_object_with_success(self, mock_session, make_mock_response):
+    @pytest.mark.parametrize(
+        'group_channels_by, group_channels_threshold_ms',
+        [(None, None), ('sentence', 5000), ('word', 2000)]
+    )
+    def test_get_transcript_object_with_success(
+        self,
+        mock_session,
+        make_mock_response,
+        group_channels_by,
+        group_channels_threshold_ms
+    ):
         data = {
             'monologues': [{
                 'speaker': 1,
@@ -142,12 +210,15 @@ class TestTranscriptEndpoints():
         expected_headers.update(client.default_headers)
         response = make_mock_response(url=URL, json_data=data)
         mock_session.request.return_value = response
+        expected_url = URL
+        if group_channels_by and group_channels_threshold_ms:
+            expected_url += f"?group_channels_by={group_channels_by}&group_channels_threshold_ms={group_channels_threshold_ms}"
 
-        res = client.get_transcript_object(JOB_ID)
+        res = client.get_transcript_object(JOB_ID, group_channels_by=group_channels_by, group_channels_threshold_ms=group_channels_threshold_ms)
 
         assert res == expected
         mock_session.request.assert_called_once_with(
-            "GET", URL, headers=expected_headers)
+            "GET", expected_url, headers=expected_headers)
 
     @pytest.mark.parametrize('id', [None, ''])
     def test_get_transcript_object_with_no_job_id(self, id, mock_session):
